c9bab54cec511ec90b5aa118f2fcdd17e441c37e
ROBERTO <UO277068@uniovi.es>
2021-10-23 17:46:37 +0200

    Cambio leve

--- src/igu/action/AddCausasAction.java
+++ src/igu/action/AddCausasAction.java
@@ -3,7 +3,7 @@ package igu.action;
 import Logica.BusinessFactory;
 import Logica.crud.CitaCrudService;
 
-public class AddCausasAction {
+public class AddCausasAction {//G
 
 	 private CitaCrudService service = BusinessFactory.forCitaCrudService();
 	 private String id;
